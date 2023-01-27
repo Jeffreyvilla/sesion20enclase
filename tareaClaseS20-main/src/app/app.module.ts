e9f553ac15404279190bcc946ee13af6e7049b16
Jeffreyvilla <118560016+Jeffreyvilla@users.noreply.github.com>
2023-01-26 22:20:24 -0500

    Add files via upload

---
+++ tareaClaseS20-main/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { InterceptorService } from './services/interceptor.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
+  ],
+  providers: [{provide:HTTP_INTERCEPTORS, useClass:InterceptorService, multi:true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
